ba0728a03e70296c07412ad22678dc65c0c01595
Thiggz <karthik93@outlook.sg>
2022-03-29 12:48:31 +0800

    model functions included

---
+++ src/machine_learning/models.py
@@ -0,0 +1,36 @@
+from imblearn.ensemble import BalancedBaggingClassifier
+
+from sklearn.tree import DecisionTreeClassifier
+
+from sklearn.ensemble import RandomForestClassifier
+
+from sklearn.neighbors import KNeighborsClassifier
+
+def knn(x_train, y_train, x_test):
+    classifier = KNeighborsClassifier(n_neighbors=10)
+    classifier.fit(x_train, y_train)
+
+    preds = classifier.predict(x_test)
+
+    return preds
+
+
+def balanced_bagging(x_train, y_train, x_test):
+    classifier = BalancedBaggingClassifier(base_estimator=DecisionTreeClassifier(),
+                                           sampling_strategy='not majority',
+                                           replacement=False,
+                                           random_state=42)
+    classifier.fit(x_train, y_train)
+
+    preds = classifier.predict(x_test)
+
+    return preds
+
+
+def random_forest(x_train, y_train, x_test):
+    classifier = RandomForestClassifier(random_state=0)
+    classifier.fit(x_train, y_train)
+
+    preds = classifier.predict(x_test)
+
+    return preds
--- src/main.py
+++ src/main.py
@@ -1,3 +1,41 @@
 import pandas as pd
+import os
+import sqlite3
+import yaml
+from machine_learning import models
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+from preprocessing import preprocess
 
-print("hello world")
+
+with open('config.YAML') as file:
+    config = yaml.safe_load(file)
+
+print(config["ordinal"])
+
+path = os.getcwd()
+parent = os.path.dirname(path)
+path = os.path.join(parent, config["database"])
+
+
+conn = sqlite3.connect(path)
+df = pd.read_sql_query("SELECT * from survive", conn)
+
+data = preprocess.PreProcess(df)
+print(data.col_abs)
+
+y = data.preprocessed["Survive"]
+x = data.preprocessed.drop("Survive", axis=1)
+
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=config["test_size"], random_state=30)
+
+knn_preds = models.knn(x_train, y_train, x_test)
+bbpreds = models.balanced_bagging(x_train, y_train, x_test)
+rfpreds = models.random_forest(x_train, y_train, x_test)
+
+knn_acc = accuracy_score(y_test, knn_preds)
+bb_acc = accuracy_score(y_test, bbpreds)
+rf_acc = accuracy_score(y_test, rfpreds)
+
+print("Accuracy: KNN: ", knn_acc, "Balanced Bagging: ", bb_acc, "Random Forest: ", rf_acc)
+

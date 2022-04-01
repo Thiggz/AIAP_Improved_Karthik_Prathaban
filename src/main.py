12446abe0475721b6e671d779e11f4463cf224c0
Thiggz <karthik93@outlook.sg>
2022-04-01 13:32:23 +0800

    updated main to obtain metrics from model_eval

--- src/main.py
+++ src/main.py
@@ -4,38 +4,54 @@ import sqlite3
 import yaml
 from machine_learning import models
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import accuracy_score
 from preprocessing import preprocess
+from evaluation import model_eval
 
+## Read config file
 
 with open('config.YAML') as file:
     config = yaml.safe_load(file)
 
-print(config["ordinal"])
+## Open the database
 
 path = os.getcwd()
 parent = os.path.dirname(path)
 path = os.path.join(parent, config["database"])
 
-
 conn = sqlite3.connect(path)
 df = pd.read_sql_query("SELECT * from survive", conn)
 
-data = preprocess.PreProcess(df)
-print(data.col_abs)
+## Perform preprocessing
 
-y = data.preprocessed["Survive"]
-x = data.preprocessed.drop("Survive", axis=1)
+print('DataFrame open, Performing Preprocessing\n')
+data = preprocess.PreProcess(df)
+
+y = data.preprocessed[config['dependent']]
+x = data.preprocessed.drop(config['dependent'], axis=1)
 
 x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=config["test_size"], random_state=30)
 
-knn_preds = models.knn(x_train, y_train, x_test)
-bbpreds = models.balanced_bagging(x_train, y_train, x_test)
-rfpreds = models.random_forest(x_train, y_train, x_test)
+## Train classifiers
 
-knn_acc = accuracy_score(y_test, knn_preds)
-bb_acc = accuracy_score(y_test, bbpreds)
-rf_acc = accuracy_score(y_test, rfpreds)
+print('Training KNN, Balanced Bagging and Random Forest Classifiers')
+knn_trained = models.knn(x_train, y_train)
+bb_trained = models.balanced_bagging(x_train, y_train)
+rf_trained = models.random_forest(x_train, y_train)
+print('Training complete\n')
 
-print("Accuracy: KNN: ", knn_acc, "Balanced Bagging: ", bb_acc, "Random Forest: ", rf_acc)
+## Test classifiers and print default evaluation metrics
 
+if config["test_size"] > 0:
+
+    print('Test set available, testing models\n')
+
+    knn_preds = knn_trained.predict(x_test)
+    bb_preds = bb_trained.predict(x_test)
+    rf_preds = rf_trained.predict(x_test)
+
+    predictions = {'K-Nearest Neighbours': knn_preds,
+                   'Balanced Bagging': bb_preds,
+                   'Random Forest': rf_preds}
+
+    for key in predictions:
+        model_eval.default_metrics(y_test, predictions[key], key)
